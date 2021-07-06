1799f9cb2812702edbd74a65f50c267fafae3a6b
Matheus Silva <matheushds.br@gmail.com>
2021-07-06 18:44:45 -0300

    style(global): add theme yoga and font-family rubik

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,6 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
+import { theme } from '@gympass/yoga';
 import reset from 'styled-reset';
 
 export const GlobalStyle = createGlobalStyle`
   ${reset}
+
+  body {
+    font-family: 'Rubik', sans-serif;
+    background-color: ${theme.colors.stamina};
+  }
 `;
