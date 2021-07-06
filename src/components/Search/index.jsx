9f8b421cdedbbafe1335c5db32a0d9b3da454319
Matheus Silva <matheushds.br@gmail.com>
2021-07-06 18:51:10 -0300

    feat(search): import input component

---
+++ src/components/Search/index.jsx
@@ -0,0 +1,10 @@
+import InputGympass from '../Input';
+import { Container } from './styles';
+
+const Search = () => (
+  <Container>
+    <InputGympass />
+  </Container>
+);
+
+export default Search;
