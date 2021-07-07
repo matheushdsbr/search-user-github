6ffda1192e1bace3e63376dc2698f7e7aa8c9a6b
Matheus Silva <matheushds.br@gmail.com>
2021-07-07 11:41:26 -0300

    feat(index): add user state and import search and resultsearch component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,19 @@
 import { ThemeProvider } from '@gympass/yoga';
+import { useState } from 'react';
 import { GlobalStyle } from '../styles/global';
 import Search from '../components/Search';
+import ResultSearch from '../components/ResultSearch';
 
-const Home = () => (
-  <ThemeProvider>
-    <GlobalStyle />
-    <Search />
-  </ThemeProvider>
-);
+const Home = () => {
+  const [user, setUser] = useState(undefined);
+
+  return (
+    <ThemeProvider>
+      <GlobalStyle />
+      <Search setUser={setUser} />
+      <ResultSearch user={user} />
+    </ThemeProvider>
+  );
+};
 
 export default Home;
