d883b8cb95e4a13cd76dfc776c123e4190b6e1db
Matheus Silva <matheushds.br@gmail.com>
2021-07-07 11:37:58 -0300

    feat(search): filter data

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,10 +1,36 @@
-import InputGympass from '../Input';
+import { useState } from 'react';
+import { Input } from '@gympass/yoga';
 import { Container } from './styles';
 
-const Search = () => (
-  <Container>
-    <InputGympass />
-  </Container>
-);
+const Search = ({ setUser }) => {
+  const [value, setValue] = useState('');
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+
+    fetch(`https://api.github.com/users/${value}`)
+      .then((response) => response.json())
+      .then((data) => setUser({
+        name: data.name,
+        bio: data.bio,
+        avatar: data.avatar_url,
+        url: data.html_url,
+      }));
+
+    setValue('');
+  };
+
+  return (
+    <Container onSubmit={handleSearch}>
+      <Input
+        label="Search user github"
+        value={value}
+        full
+        onChange={(e) => setValue(e.target.value)}
+        onClean={(cleaned) => setValue(cleaned)}
+      />
+    </Container>
+  );
+};
 
 export default Search;
--- src/components/Search/styles.js
+++ src/components/Search/styles.js
@@ -1,5 +1,26 @@
 import styled from 'styled-components';
+import { theme } from '@gympass/yoga';
 
-export const Container = styled.div`
+export const Container = styled.form`
   padding: 30px;
+
+  fieldset {
+    border-color: ${theme.colors.medium};
+
+    &:hover, &:focus-within {
+      border-color: ${theme.colors.clear};
+    }
+
+    input:focus {
+      color: ${theme.colors.clear};
+
+      & ~ label {
+        color: ${theme.colors.clear};
+      }
+    }
+
+    label {
+      color: ${theme.colors.medium};
+    }
+  }
 `;
