03cb795ecaf47ddd2b16956ee38cf5a6e0cadce9
Matheus Silva <matheushds.br@gmail.com>
2021-07-09 12:32:47 -0300

    feat(userdata): add repos and reposStar

--- src/providers/userData.js
+++ src/providers/userData.js
@@ -3,10 +3,16 @@ import { createContext, useState, useContext } from 'react';
 export const UserDataContext = createContext({});
 
 export const UserDataProvider = ({ children }) => {
+  const [value, setValue] = useState('');
   const [user, setUser] = useState(undefined);
+  const [repos, setRepos] = useState([]);
+  const [reposStar, setReposStar] = useState([]);
 
   return (
-    <UserDataContext.Provider value={{ user, setUser }}>
+    <UserDataContext.Provider value={{
+      value, setValue, user, setUser, repos, setRepos, reposStar, setReposStar,
+    }}
+    >
       {children}
     </UserDataContext.Provider>
   );
