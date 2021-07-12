2128b12962de2967fad89f9cca28e7df9796659a
Matheus Silva <matheushds.br@gmail.com>
2021-07-12 17:03:51 -0300

    style(resultrepos): add styles to repos

---
+++ src/components/ResultRepos/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import { theme } from '@gympass/yoga';
+
+export const Container = styled.div`
+  border-bottom: 1px solid #d1d5da;
+  padding-bottom: ${theme.spacing.large}px;
+  padding-top: ${theme.spacing.large}px;
+
+  h3 {
+    font-size: ${theme.fontSizes.large}px;
+    a {
+      color: #0366d6;
+      text-decoration: none;
+
+      &:hover {
+        color: #0366d6;
+        text-decoration: underline;
+      }
+    }
+  }
+
+  p {
+    font-size: ${theme.fontSizes.small}px;
+    color: #586069;
+  }
+
+  span {
+    font-size: ${theme.fontSizes.xsmall}px;
+    color: #586069;
+  }
+`;
