d1e74f67b5543ce7b09087d7b5ed2a70ddd3da92
Matheus Silva <matheushds.br@gmail.com>
2021-07-13 07:12:56 -0300

    feat(search): import ErrorMessage component

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import axios from 'axios';
 import { Input } from '@gympass/yoga';
+import ErrorMessage from '../ErrorMessage';
 import { Container } from './styles';
 import { useUserDataContext } from '../../providers/userData';
 
@@ -7,48 +9,16 @@ const Search = () => {
   const {
     value, setValue, setUser, setRepos, setReposStar,
   } = useUserDataContext();
+  const [message, setMessage] = useState(false);
 
   const handleSearch = (event) => {
     event.preventDefault();
 
-    // fetch(`https://api.github.com/users/${value}`)
-    //   .then((response) => {
-    //     if (response.status === 404) {
-    //       return alert('User Not Found');
-    //     }
-
-    //     if (!response.ok) {
-    //       return alert('Request Failed');
-    //     }
-
-    //     return response.json();
-    //   })
-    //   .then((data) => {
-    //     if (data !== undefined) {
-    //       setUser({
-    //         name: data.name,
-    //         bio: data.bio,
-    //         avatar: data.avatar_url,
-    //         url: data.html_url,
-    //         repos: data.public_repos,
-    //         location: data.location,
-    //         followers: data.followers,
-    //         following: data.following,
-    //       });
-
-    //       fetch(`https://api.github.com/users/${value}/repos`)
-    //         .then((response) => response.json())
-    //         .then((repos) => setRepos(repos));
-
-    //       fetch(`https://api.github.com/users/${value}/starred`)
-    //         .then((response) => response.json())
-    //         .then((starred) => setReposStar(starred));
-    //     }
-    //   });
-
     axios.get(`https://api.github.com/users/${value}`)
       .then((response) => {
         if (response.status === 200) {
+          setMessage(false);
+
           const { data } = response;
           setUser({
             name: data.name,
@@ -68,8 +38,8 @@ const Search = () => {
             .then((responseStarred) => setReposStar(responseStarred.data));
         }
       })
-      .catch((error) => {
-        alert(error);
+      .catch(() => {
+        setMessage(true);
       });
   };
 
@@ -79,9 +49,12 @@ const Search = () => {
         label="Search GitHub user..."
         value={value}
         full
+        error={message}
         onChange={(e) => setValue(e.target.value)}
         onClean={(cleaned) => setValue(cleaned)}
       />
+
+      {message && <ErrorMessage />}
     </Container>
   );
 };
